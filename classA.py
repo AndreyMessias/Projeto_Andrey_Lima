1fcf75621cfe278dca7935e76a0c66a2d837cc5c
Andrey <andreylima7868@gmail.com>
2026-04-16 17:11:24 -0300

    Adicionando classes A e B com métodos e atributos

---
+++ classA.py
@@ -0,0 +1,15 @@
+class ClasseA:
+    def __init__(self):
+        self.A1 = 0      # int
+        self.A2 = 0.0    #float
+    # Gets e Sets
+    def get_A1(self): return self.A1
+    def set_A1(self, valor): self.A1 = valor
+    def get_A2(self): return self.A2
+    def set_A2(self, valor): self.A2 = valor
+
+    def MA1(self):
+        print("MA1")
+
+    def MA2(self):
+        print("MA2")
---
+++ classeB.py
@@ -0,0 +1,16 @@
+class ClasseB:
+    def __init__(self):
+        self.B1 = 0      # int
+        self.B2 = 0.0    # float
+
+    # Gets e Sets
+    def get_B1(self): return self.B1
+    def set_B1(self, valor): self.B1 = valor
+    def get_B2(self): return self.B2
+    def set_B2(self, valor): self.B2 = valor
+
+    def MB1(self):
+        print("MB1")
+
+    def MB2(self):
+        print("MB2")
