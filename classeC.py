7525bcf22958a1edce467fef96a76d12d0af1007
Andrey <andreylima7868@gmail.com>
2026-04-16 18:13:54 -0300

    Criação da Classe C

---
+++ classeC.py
@@ -0,0 +1,18 @@
+class ClasseC:
+    def __init__(self):
+        self.C1 = ""     
+        self.C2 = 0    
+
+ 
+    def get_C1(self): return self.C1
+    def set_C1(self, valor): self.C1 = valor
+
+    def get_C2(self): return self.C2
+    def set_C2(self, valor): self.C2 = valor
+
+   
+    def MC1(self):
+        print("MC1")
+
+    def MC2(self):
+        print("MC2")
